34a80cec3b945cebbe43a06a29f37f51f17c6a9b
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2025-03-05 01:49:43 +0600

    Update animated-headline.js

--- assets/js/animated-headline.js
+++ assets/js/animated-headline.js
@@ -2,8 +2,8 @@ jQuery(document).ready(function($){
 	//set animation timing
 	var animationDelay = 2500,
 		//loading bar effect
-		barAnimationDelay = 3800,
-		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+		barAnimationDelay = 100,
+		barWaiting = barAnimationDelay - 50, //3000 is the duration of the transition on the loading bar - set in the scss/css file
 		//letters effect
 		lettersDelay = 50,
 		//type effect
